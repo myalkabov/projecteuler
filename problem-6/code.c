88c352aaa6325f981ea9237eed122aaaf821dff1
Maksat Yalkabov <yalkabovmaksat@gmail.com>
2020-01-15 01:22:26 +0200

    removed math.h

--- problem-6/code.c
+++ problem-6/code.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <math.h>
 #define N 100
 
 int
 main()
 {
     int sum1, // 1^2 + 2^2 + 3^2 +...+ N^2
-        sum2, // 1 + 2 + 3 + ... + N
-        ans;
+        sum2; // 1 + 2 + 3 + ... + N
     
     sum1 = N*(N+1)*(2*N+1)/6;
     sum2 = N*(N+1)/2;
