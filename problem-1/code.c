57dfa1b95f70ba4b4c0bad5f2b74891c8cdc06eb
myalkabov <yalkabovmaksat@gmail.com>
2020-01-15 00:21:45 +0200

    indent size changed to 4

--- problem-1/code.c
+++ problem-1/code.c
@@ -7,8 +7,8 @@ int main()
     int n2 = n / 5;  // 1,2,3,..,n2 or 5,10,15,..,5n2 in 1..n
     int n3 = n / 15; // 1,2,3,..,n3 or 15,30,45,..,15n3 in 1..n
     int sum = 3  * (n1 * (n1 + 1) / 2) + // sum of mults. of 3  in 1..n
-	      5  * (n2 * (n2 + 1) / 2) - // sum of mults. of 5  in 1..n
-	      15 * (n3 * (n3 + 1) / 2);  // sum of mults. of 15 in 1..n
+	  		  5  * (n2 * (n2 + 1) / 2) - // sum of mults. of 5  in 1..n
+	      	  15 * (n3 * (n3 + 1) / 2);  // sum of mults. of 15 in 1..n
     printf("%d\n", sum);
     return 0;
 }
